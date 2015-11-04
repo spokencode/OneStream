98eea659cfa626f4598ef822a619c9c74752b9a8
Jason Renai <jason@JasonsMacBook.local>
2015-11-03 22:41:31 -0500

    added footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,23 @@
 <footer class="content-info">
   <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+    <div class="row">
+       <div class="col-xs-12">
+        <?php dynamic_sidebar('sidebar-footer'); ?>
+    </div>
+    </div>
+  </div>
+  <div id="onestream-footer" class="container">
+    <div id="footer-social" class="row">
+      <div class="col-md-3 text-center"><i class="fa fa-linkedin"></i></div>
+      <div class="col-md-3 text-center"><i class="fa fa-twitter"></i></div>
+      <div class="col-md-3 text-center"><i class="fa fa-google"></i></div>
+      <div class="col-md-3 text-center"><i class="fa fa-youtube"></i></div>
+    </div>
+    <div id="footer-links" class="row">
+      <div class="col-md-12 text-center">
+        <a href="http://www.onestreamsoftware.com/company/careers-2/">Careers</a> | <a href="http://www.onestreamsoftware.com/legal-notices/">Legal Notices</a>
+        ©2015 OneStream Software LLC All Rights Reserved
+      </div>
+    </div>
   </div>
 </footer>
--- templates/head.php
+++ templates/head.php
@@ -4,5 +4,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700' rel='stylesheet' type='text/css'>
   <link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,300italic,700' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
   <?php wp_head(); ?>
 </head>
